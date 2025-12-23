b884d4f02306e1a46fdb68692d71dec83f4f2562
Tom Pritchard <tom@tpritc.com>
2025-12-23 20:02:57 +0000

    Minimal design

--- app/helpers/style_helper.rb
+++ app/helpers/style_helper.rb
@@ -1,5 +1,5 @@
 module StyleHelper
   def field_classes
-    "mt-1 block w-full h-10 rounded-md border border-gray-300 bg-white px-3 py-2 focus:border-indigo-500 focus:ring-indigo-500 focus:outline-none"
+    "mt-1 block w-full h-9 rounded border border-stone-300 bg-white px-3 py-2 text-sm focus:border-stone-500 focus:outline-none"
   end
 end
