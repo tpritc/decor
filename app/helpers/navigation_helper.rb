0d57f36f4642e3af33463cb3fe710db94c489aa0
Tom Pritchard <tom@tpritc.com>
2025-12-29 11:26:19 +0000

    Simplify filters for computers

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -3,8 +3,9 @@ module NavigationHelper
     active_link_to(
       name,
       path,
-      class: "font-medium text-stone-700 hover:text-stone-900",
-      class_active: "mt-0.5 border-b-2 border-decor",
+      class: "mt-0.5 font-medium text-stone-700 hover:text-stone-900 border-b-2 hover:border-decor-lighter",
+      class_active: "border-decor",
+      class_inactive: "border-transparent",
       **options
     )
   end
