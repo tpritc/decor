2ba5f204d5c97acf5c244624dde7825deca0c084
Rob Pritchard <rob_pritchard@outlook.com>
2026-01-25 21:18:10 +0000

    updated app email address for new domain

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: email_address_with_name("decor@tpritc.com", "Decor")
+  default from: email_address_with_name("admin@decorweb.net", "Decor")
   layout "mailer"
   default template_path: -> { "mailers/#{self.class.name.underscore}" }
 end
